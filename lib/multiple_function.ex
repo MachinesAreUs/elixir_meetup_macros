75b63d5108aa1061c7f5c17f856963c88b98841c
Agustin Ramos Fonseca <zentimental.software@gmail.com>
2015-07-22 16:36:11 -0500

    separated complex expression

--- lib/multiple_function.ex
+++ lib/multiple_function.ex
@@ -4,10 +4,10 @@ defmodule MultipleFunction do
     
     quote do
       ["_1","_2"] 
-        |> Enum.map(&((to_string(unquote(base_name)) <> &1) |> String.to_atom))
+        |> Enum.map(&(to_string(unquote(base_name)) <> &1))
+        |> Enum.map(&String.to_atom/1)
         |> Enum.map(&Macro.escape/1)
         |> Enum.each fn(name) -> 
-          #def unquote(name)() do
           def name() do
             unquote(block)
           end
