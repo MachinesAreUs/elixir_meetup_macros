12e5c56d94a30e8c34d25cb712d153bbf1677154
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2015-07-25 00:26:26 -0500

    added several tests

---
+++ test/macros_test.exs
@@ -0,0 +1,60 @@
+defmodule MacrosTest do
+  use ExUnit.Case
+
+  test "quotations returns a tuple representing the AST of the expression" do
+    assert (quote do: 1 + 2) == {:+, [context: MacrosTest, import: Kernel], [1, 2]}
+  end
+
+  test "you can evaluate a quoted expression with 'Code.eval_quoted'" do
+    ast = quote do: 1 + 2
+    {val, bindings} = Code.eval_quoted ast
+    assert val == 3
+    assert bindings == []
+  end
+
+  test "unquote allows to introduce AST fragments within a quoted expression" do
+    ast = quote do: "hola"
+    ast2 = quote do: String.reverse unquote(ast)
+    {val,_} = Code.eval_quoted ast2
+    assert val == "aloh"
+  end
+
+  test "define a simple function through a macro" do
+    
+    defmodule TestModule do
+      require SimpleFunction
+      SimpleFunction.new_function :hello, do: "world"
+      SimpleFunction.new_function :foo,   do: "bar"
+    end
+
+    assert TestModule.hello == "world"
+    assert TestModule.foo   == "bar"
+  end
+ 
+  test "define multiple functions through a singl macro call" do
+
+    defmodule TestModule do
+      require MultipleFunction
+      MultipleFunction.new_functions :hello, do: "ok"
+    end
+    
+    #assert TestModule.hello_1 == "ok" 
+    #assert TestModule.hello_2 == "ok" 
+    assert TestModule.name == "ok" 
+  end
+
+  test "You can define methods for a module at compile time" do
+    import StateMachine
+    assert initial |> pause == :paused
+    assert initial |> pause |> resume == :running
+    assert initial |> pause |> resume |> stop == :stopped
+  end
+
+  test "you can define an API for Github at compile time" do
+    repos = Github.__info__(:functions) |> Enum.map &elem(&1,0)
+    assert Enum.member?(repos, :elixir_meetup_macros)
+    assert Enum.member?(repos, :elm_playground)
+    assert Enum.member?(repos, :elixir_playground)
+  end
+
+end
